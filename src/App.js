75505097b34d7573acc467360cedb94a2b98ac8e
Julia Lipniewicz <julia.modex21@gmail.com>
2024-01-18 15:26:29 +0100

    creating css file, adding static search field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useEffect } from "react"; 
+import './App.css';
+import SearchIcon from './search.svg';
+
+// 116b5210
+
+const API_URL = 'http://www.omdbapi.com?apikey=116b5210';
+
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch (`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+
+useEffect(()=> {
+  searchMovies('Spiderman');
+
+},[]);
+
   return (
     <div className="App">
-   <h1>App</h1>
+   <h1>MovieEngine</h1>
+
+   <div className="search">
+    <input
+    placeholder="Search for movies"
+    value='Superman'
+    onChange={()=> {}}
+    />
+   </div>
     </div>
   );
 }
 
 export default App;
+
+
