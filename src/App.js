01870b55b923112ea3d7dded1d0a11fa3d29fe23
Julia Lipniewicz <julia.modex21@gmail.com>
2024-01-18 16:47:20 +0100

    creating a static movie1 Test in App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=116b5210';
 
+const movie1 = {
+  "Title": "Italian Spiderman",
+  "Year": "2007",
+  "imdbID": "tt2705436",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+}
+
+
+
 function App() {
 
   const searchMovies = async (title) => {
@@ -30,7 +40,25 @@ useEffect(()=> {
     value='Superman'
     onChange={()=> {}}
     />
-   </div>
+    <img src={SearchIcon}
+    alt='search'
+    onClick={()=>{}}
+    />
+    <div className="container">
+      <div className="movie">
+         <div>
+          <p>{movie1.Year}</p>
+         </div>
+         <div>
+          <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+         </div>
+         <div>
+          <span>{movie1.Type}</span>
+          <h3>{movie1.Title}</h3>
+         </div>
+      </div>
+    </div>
+    </div>
     </div>
   );
 }
